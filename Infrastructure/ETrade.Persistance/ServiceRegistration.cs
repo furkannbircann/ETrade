f7b08fc8edde8ae62176bb15f48082f6c91e1718
Furkan Bircan <furkhanbircan@gmail.com>
2023-01-11 18:15:01 +0300

    Add project files.

---
+++ Infrastructure/ETrade.Persistance/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETrade.Application.Abstractions;
+using ETrade.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETrade.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
---
+++ Presentation/ETrade.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ETrade.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETrade.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = _productService.GetAll();
+            return Ok(products);
+        }
+    }
+}
+ 
