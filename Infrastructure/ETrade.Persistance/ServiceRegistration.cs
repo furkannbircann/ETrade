78466e962ab99830ce8127716267dd7870aa3fec
Furkan Bircan <furkhanbircan@gmail.com>
2023-02-10 18:22:58 +0300

    Migration - 1

--- Infrastructure/ETrade.Persistance/ServiceRegistration.cs
+++ Infrastructure/ETrade.Persistance/ServiceRegistration.cs
@@ -1,11 +1,13 @@
 ﻿using ETrade.Application.Abstractions;
 using ETrade.Persistance.Concretes;
+using ETrade.Persistance.Contexts;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETrade.Persistance
 {
@@ -13,7 +15,8 @@ namespace ETrade.Persistance
     {
         public static void AddPersistanceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETradeContext>(options => options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ETradeDb;"));
         }
     }
 }
+  
